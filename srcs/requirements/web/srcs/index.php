f2ecb00212af81b97adf8e32d82c438b6ca89c8a
Rart3x <ramjattankeny971@gmail.com>
2024-02-14 03:52:58 +0100

    Rework front & back to web, .htaccess on work, following freeskillz tutos

---
+++ srcs/requirements/web/srcs/index.php
@@ -0,0 +1,7 @@
+<?php
+    session_start();
+    define('DS', DIRECTORY_SEPARATOR);
+    echo DS;
+    define('ROOT', dirname(__FILE__));
+    echo $_SERVER['PATH_INFO'];
+    $url = isset($_SERVER['PATH_INFO']) ? explode ('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
