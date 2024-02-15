93e2601b1c13242231b6b427dbbb34981ba316cf
Rart3x <ramjattankeny971@gmail.com>
2024-02-15 06:28:12 +0100

    add CRUD functions

--- srcs/requirements/web/srcs/core/DB.php
+++ srcs/requirements/web/srcs/core/DB.php
@@ -13,6 +13,10 @@
             }
         }
 
+        public function error() {
+            return $this->_error;
+        }
+
         public static function getInstance() {
             if (!isset(self::$_instance))
                 self::$_instance = new DB();
@@ -38,4 +42,45 @@
             }
             return $this;
         }
+
+        public function insert($table, $fields = []) {
+            $fieldString = '';
+            $valueString = '';
+            $values = [];
+            foreach ($fields as $field => $value) {
+                $fieldString .= '`' . $field . '`,';
+                $valueString .= '?,';
+                $values[] = $value;
+            }
+            $fieldString = rtrim($fieldString, ',');
+            $valueString = rtrim($valueString, ',');
+            $sql = "INSERT INTO {$table} ({$fieldString}) VALUES ({$valueString})";
+            if (!$this->query($sql, $values)->error())
+                return true;
+            return false;
+        }
+
+        public function update($table, $id, $fields = []) {
+            $fieldString = '';
+            $values = [];
+            foreach ($fields as $field => $value) {
+                $fieldString .= ' ' . $field . ' = ?,';
+                $values[] = $value;
+            }
+            $fieldString = trim($fieldString);
+            $fieldString = rtrim($fieldString, ',');
+            $sql = "UPDATE {$table} SET {$fieldString} WHERE id = {$id}";
+            if (!$this->query($sql, $values)->error())
+                return true;
+            return false;
+        }
+
+        public function delete($table, $id) {
+            $sql = "DELETE FROM {$table} WHERE id = {$id}";
+            if (!$this->query($sql)->error())
+                return true;
+            return false;
+        }
+
+
     }
