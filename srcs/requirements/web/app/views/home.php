75ea9c5f06ee68d20996d932661c915b292290b4
Rart3x <ramjattankeny971@gmail.com>
2024-02-16 18:49:21 +0100

    add registration route

--- srcs/requirements/web/app/views/home.php
+++ srcs/requirements/web/app/views/home.php
@@ -6,7 +6,7 @@
 
         <title>Camagru</title>
 
-        <link rel="stylesheet" href="<?php ROOT . DS . 'style' . DS . 'main.css' ?>">
+        <link rel="stylesheet" href="../../style/main.css">
 
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@^2.2.19/dist/tailwind.min.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/daisyui@^1.3.6/dist/full.css" rel="stylesheet">
---
+++ srcs/requirements/web/app/views/registration.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html data-theme="retro" lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Camagru</title>
+
+        <link rel="stylesheet" href="../../style/main.css">
+
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@^2.2.19/dist/tailwind.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@^1.3.6/dist/full.css" rel="stylesheet">
+    </head>
+    <body>
+        <input class="btn" type="text">
+        <input class="btn" type="password">
+    </body>
+
+</html>
