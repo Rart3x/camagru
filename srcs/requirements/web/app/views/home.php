645dd1123a0958738a1c54daab43f80fd3d10e66
Rart3x <ramjattankeny971@gmail.com>
2024-02-17 10:37:05 +0100

    front for home, login, resgistration (Tailwind CSS components)

--- srcs/requirements/web/app/views/home.php
+++ srcs/requirements/web/app/views/home.php
@@ -12,42 +12,55 @@
         <link href="https://cdn.jsdelivr.net/npm/daisyui@^1.3.6/dist/full.css" rel="stylesheet">
     </head>
     <body>
-        <header class="text-white p-4">
-            <nav>
-                <ul>
-                    <div class="align-center">
-                        <button><a href="#home" class="btn btn">Home</a></li>
-                        <button><a href="#about" class="btn btn-secondary">About</a></li>
-                        <button><a href="#gallery" class="btn btn-accent">Galery</a></li>
+        <div class="flex h-screen bg-green-300">
+            <div class="flex-1 flex flex-col overflow-hidden">
+              <header class="flex justify-between items-center bg-blue-300 p-4">
+                <div class="flex">Left</div>
+                <div class="flex">Right</div>
+              </header>
+              <div class="flex h-full">
+                <nav class="flex w-72 h-full bg-pink-500">
+                  <div class="w-full flex mx-auto px-6 py-8">
+                    <div class="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed">Sidebar</div>
+                  </div>
+                </nav>
+                <main class="flex flex-col w-full bg-white overflow-x-hidden overflow-y-auto mb-14">
+                  <div class="flex w-full mx-auto px-6 py-8">
+                    <div class="flex flex-col w-full h-full text-gray-900 text-xl border-4 border-gray-900 border-dashed">
+                      <div class="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
+                      <div class="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
+                      <div class="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
+                      <div class="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
+                      <div class="flex w-full max-w-xl h-60 items-center justify-center mx-auto bg-green-400 border-b border-gray-600">Post</div>
                     </div>
-                </ul>
-            </nav>
-        </header>
-        <div class="left-part">
-            <p>left</p>
-        </div>
-        <div class="center-part">
-            <div class="center-part-header">
-                <div class="center-part-header-left">
-                    <img src="assets/default_pic.jpg" alt="default pic">
-                </div>
-                <div class="center-part-header-center">
-                    <input type="text" placeholder="What's up ?">
-                </div>
-                <div class="center-part-header-right">
-                    <img src="assets/gallery.jpg" alt="gallery pic">
-                    <button type="submit">Post</button>
-                </div>
+                  </div>
+                </main>
+                <nav class="flex w-72 h-full bg-yellow-400">
+                  <div class="w-full flex mx-auto px-6 py-8">
+                    <div class="w-full h-full flex items-center justify-center text-gray-900 text-xl border-4 border-gray-900 border-dashed">Rightbar</div>
+                  </div>
+                </nav>
+              </div>
             </div>
-            <p>center</p>
-        </div>
-        <div class="right-part">
-            <p>right</p>
-        </div>
-
-         <footer class="bg-gray-800 text-white p-4">
-            <p class="text-center">&copy; Rart3x</p>
-            <p>footer</p>
-        </footer>
+          </div>
+          
+          <style>
+          ::-webkit-scrollbar {
+            width: 5px;
+            height: 5px;
+          }
+          ::-webkit-scrollbar-thumb {
+            background: linear-gradient(13deg, #7bcfeb 14%, #e685d3 64%);
+            border-radius: 10px;
+          }
+          ::-webkit-scrollbar-thumb:hover {
+            background: linear-gradient(13deg, #c7ceff 14%, #f9d4ff 64%);
+          }
+          ::-webkit-scrollbar-track {
+            background: #ffffff;
+            border-radius: 10px;
+            box-shadow: inset 7px 10px 12px #f0f0f0;
+          }
+          </style>
     </body>
 </html>
